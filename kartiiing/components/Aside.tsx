3c88ab041caaa27806c97f9c442c6f118ed12ff0
Boldizsar Szabo <szboldisz@gmail.com>
2025-04-30 14:46:01 +0200

    Update dependencies in package.json and other small changes

--- kartiiing/components/Aside.tsx
+++ kartiiing/components/Aside.tsx
@@ -2,15 +2,21 @@ interface Props {
   children?: React.ReactNode;
   position: "left" | "right";
   visibilityFrom: "lg" | "xl";
+  className?: string;
 }
 
-const Aside = ({ children, position, visibilityFrom }: Props) => {
+const Aside = ({
+  children,
+  position,
+  visibilityFrom,
+  className = "",
+}: Props) => {
   const borderClass = position === "left" ? "border-r" : "border-l";
   const visibilityClass = visibilityFrom === "lg" ? "lg:block" : "xl:block";
 
   return (
     <aside
-      className={`w-3xs hidden ${visibilityClass} py-3 z-30 shrink-0 border-dashed ${borderClass}`}
+      className={`w-3xs hidden ${visibilityClass} py-3 z-30 shrink-0 border-dashed ${borderClass} ${className}`}
     >
       {children}
     </aside>

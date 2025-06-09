c48bf9899004714aa5e23adbbd363feeb57a874e
Boldizsar Szabo <boldizsar.szabo@rabit.hu>
2025-06-09 10:20:35 +0200

    Update StatusBadge styling

--- kartiiing/components/calendar/StatusBadge.tsx
+++ kartiiing/components/calendar/StatusBadge.tsx
@@ -6,12 +6,12 @@ export default function StatusBadge({ status }: Props) {
   if (!status) return null;
 
   const baseClasses =
-    "relative text-xs font-semibold uppercase pl-3 pr-2.5 py-1.5 rounded-bl-xl overflow-hidden ";
+    "relative text-xs tracking-wider uppercase pl-3.5 pr-2.5 py-2 rounded-bl-xl overflow-hidden ";
 
   const colorClasses = {
-    Live: "bg-red-500 text-white dark:bg-red-700",
-    Upcoming: "bg-red-500 text-white dark:bg-red-700",
-    Finished: "bg-zinc-950 text-white dark:bg-zinc-800",
+    Live: "bg-red-600 text-white dark:bg-red-700",
+    Upcoming: "bg-red-600 text-white dark:bg-red-700",
+    Finished: "bg-zinc-800 text-white",
   };
 
   return <span className={baseClasses + colorClasses[status]}>{status}</span>;

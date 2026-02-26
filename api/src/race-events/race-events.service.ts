fab1c6060da0857c1d3e8679bd9a90f9571c0fb6
Boldizsar Szabo <boldizsar.szabo@rabit.hu>
2026-02-26 23:00:54 +0100

    Date ordering for race events on calendar page #21

--- api/src/race-events/race-events.service.ts
+++ api/src/race-events/race-events.service.ts
@@ -233,9 +233,10 @@ export class RaceEventsService {
     qb: SelectQueryBuilder<RaceEvent>,
     sort: RaceEventSortOptions,
   ): void {
-    qb.orderBy(
-      'raceEvent.dateStart',
-      sort === RaceEventSortOptions.DESC ? 'DESC' : 'ASC',
+    const direction = sort === RaceEventSortOptions.DESC ? 'DESC' : 'ASC';
+    qb.orderBy('raceEvent.dateStart', direction).addOrderBy(
+      'raceEvent.dateEnd',
+      direction,
     );
   }
 

23720e52a5c12139140c3dc026238178b810f72d
Boldizsar Szabo <boldizsar.szabo@rabit.hu>
2026-03-14 15:57:40 +0100

    Refactor components and files #29

--- kartiiing/app/calendar/page.tsx
+++ kartiiing/app/calendar/page.tsx
@@ -1,8 +1,19 @@
 import { redirect } from "next/navigation";
+import { getAvailableYears } from "@/lib/api";
 
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 
 export default async function CalendarPage() {
+  const availableYears = await getAvailableYears();
   const currentYear = new Date().getFullYear();
-  redirect(`/calendar/${currentYear}`);
+  let targetYear = currentYear;
+
+  if (availableYears.length > 0) {
+    if (availableYears.includes(currentYear)) {
+      targetYear = currentYear;
+    } else {
+      targetYear = Math.max(...availableYears);
+    }
+  }
+  redirect(`/calendar/${targetYear}`);
 }

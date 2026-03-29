6c1cd51f64f6fcbc3cc428209b061f00ba4cd0e4
Boldizsar Szabo <boldizsar.szabo@rabit.hu>
2026-03-29 10:30:53 +0200

    Add tests for frontend #36

--- kartiiing/components/shared/SearchBar.tsx
+++ kartiiing/components/shared/SearchBar.tsx
@@ -7,9 +7,14 @@ import { useRef } from "react";
 type Props = {
   searchQuery: string;
   setSearchQuery: (value: string) => void;
+  placeholder?: string;
 };
 
-export default function SearchBar({ searchQuery, setSearchQuery }: Props) {
+export default function SearchBar({
+  searchQuery,
+  setSearchQuery,
+  placeholder = "Search...",
+}: Props) {
   const inputRef = useRef<HTMLInputElement>(null);
 
   function clearSearch() {
@@ -17,21 +22,35 @@ export default function SearchBar({ searchQuery, setSearchQuery }: Props) {
     inputRef.current?.focus();
   }
 
+  function handleEscapeKey(e: React.KeyboardEvent) {
+    if (e.key === "Escape") {
+      if (searchQuery) {
+        clearSearch();
+      } else {
+        inputRef.current?.blur();
+      }
+    }
+  }
+
   return (
     <div className="md:max-w-md flex items-center relative">
       <Input
-        placeholder="Search... (e.g. kz2 summer)"
+        placeholder={placeholder}
         value={searchQuery}
         onChange={(e) => setSearchQuery(e.target.value)}
+        onKeyDown={(e) => handleEscapeKey(e)}
         ref={inputRef}
         className="w-full md:w-sm h-10.5"
       />
 
       {searchQuery && (
-        <X
-          className="w-8.5 h-8.5 p-1 absolute right-1 border rounded-sm cursor-pointer text-zinc-600 dark:text-zinc-300 hover:bg-accent "
+        <button
+          aria-label="Clear search"
           onClick={clearSearch}
-        />
+          className="absolute right-1 cursor-pointer text-zinc-600 dark:text-zinc-300 hover:bg-accent border rounded-sm p-1"
+        >
+          <X className="w-6 h-6" />
+        </button>
       )}
     </div>
   );
